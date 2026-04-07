15db282ff4c6391332a6b210b507651747a52b9a
kirankarakalli <kirankarakalli92@gmail.com>
2026-04-07 10:15:04 +0530

    requirement added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='Medical_chatbot',
+    version="0.1.0",
+    author="kiran karakalli",
+    author_email="kirankarakalli92@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
