622b83131249a018ff458c9625935301c6f5af76
kirankarakalli <kirankarakalli92@gmail.com>
2026-04-07 09:53:39 +0530

    created a folder structure

---
+++ template.sh
@@ -0,0 +1,18 @@
+#Template to create a folder structure
+
+mkdir -p src
+mkdir -p research
+
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch research/__init__.py
+touch research/trials.ipynb
+touch setup.py
+touch .env
+touch app.py
+touch requirements.txt
+
+echo "Directory and files created successfully"
+
+
